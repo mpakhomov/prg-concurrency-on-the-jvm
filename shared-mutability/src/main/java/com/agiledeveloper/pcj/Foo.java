f4c58e0d5b0de5ddf44f343e5febfeaefebea335
mpakhomov <mike.pakhomov@gmail.com>
2015-06-25 19:59:33 +0300

    commited recent changes

---
+++ shared-mutability/src/main/java/com/agiledeveloper/pcj/Foo.java
@@ -0,0 +1,11 @@
+package com.agiledeveloper.pcj;
+
+/**
+ * @author mpakhomov
+ * @since: 6/25/2015
+ */
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println("foo");
+    }
+}
