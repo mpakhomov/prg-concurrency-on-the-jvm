a60ea838d89f9990ce2650d6adc2856f93be67fb
mpakhomov <mike.pakhomov@gmail.com>
2015-06-30 19:36:30 +0300

    Got to work STM using Java Multiverse library

--- using-transactional-memory/src/main/java/com/agiledeveloper/pcj/EnergySource.java
+++ using-transactional-memory/src/main/java/com/agiledeveloper/pcj/EnergySource.java
@@ -5,49 +5,97 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/vspcon for more book information.
-***/
+ ***/
 package com.agiledeveloper.pcj;
 
 //import akka.stm.Ref;
 //import akka.stm.Atomic;
+
+import org.multiverse.api.StmUtils;
+import org.multiverse.api.Txn;
+import org.multiverse.api.callables.TxnBooleanCallable;
+import org.multiverse.api.callables.TxnIntCallable;
+import org.multiverse.api.callables.TxnLongCallable;
+import org.multiverse.api.references.*;
+//import static org.multiverse.api.StmUtils.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class EnergySource {
-//  private final long MAXLEVEL = 100;
-//  final Ref<Long> level = new Ref<Long>(MAXLEVEL);
-//  final Ref<Long> usageCount = new Ref<Long>(0L);
-//  final Ref<Boolean> keepRunning = new Ref<Boolean>(true);
-//  private static final ScheduledExecutorService replenishTimer =
-//    Executors.newScheduledThreadPool(10);
-//
-//
-//  private EnergySource() {}
-//
-//  private void init() {
-//    replenishTimer.schedule(new Runnable() {
-//      public void run() {
-//        replenish();
-//        if (keepRunning.get()) replenishTimer.schedule(
-//          this, 1, TimeUnit.SECONDS);
-//      }
-//    }, 1, TimeUnit.SECONDS);
-//  }
-//
-//  public static EnergySource create() {
-//    final EnergySource energySource = new EnergySource();
-//    energySource.init();
-//    return energySource;
-//  }
-//
-//  public void stopEnergySource() { keepRunning.swap(false); }
-//
-//  public long getUnitsAvailable() { return level.get(); }
-//
-//  public long getUsageCount() { return usageCount.get(); }
-//
-//  public boolean useEnergy(final long units) {
+    private final long MAXLEVEL = 100;
+    private final TxnLong level = StmUtils.newTxnLong(MAXLEVEL);
+    private final TxnLong usageCount = StmUtils.newTxnLong(0L);
+    private final TxnBoolean keepRunning = StmUtils.newTxnBoolean(true);
+    private static final ScheduledExecutorService replenishTimer =
+            Executors.newScheduledThreadPool(10);
+
+
+    private EnergySource() {
+    }
+
+    private void init() {
+        replenishTimer.schedule(new Runnable() {
+            public void run() {
+                replenish();
+                System.out.println("keepRunning=" + keepRunning.atomicGet());
+                if (keepRunning.atomicGet()) replenishTimer.schedule(
+                        this, 1, TimeUnit.SECONDS);
+            }
+        }, 1, TimeUnit.SECONDS);
+    }
+
+    public static EnergySource create() {
+        final EnergySource energySource = new EnergySource();
+        energySource.init();
+        return energySource;
+    }
+
+    public void stopEnergySource() {
+        StmUtils.atomic(() -> keepRunning.set(false));
+        System.out.println("Running replenishTimer.shutdown()");
+        replenishTimer.shutdown();
+        try {
+            System.out.println("Running replenishTimer.shutdownNow()");
+            if (!replenishTimer.awaitTermination(30, TimeUnit.SECONDS)) {
+                replenishTimer.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+            replenishTimer.shutdownNow();
+        }
+    }
+
+    public long getUnitsAvailable() {
+        return level.atomicGet();
+//        return StmUtils.atomic(
+//                (Txn txn) -> level.get()
+//        );
+
+    }
+
+    public long getUsageCount() {
+        return usageCount.atomicGet();
+//        return StmUtils.atomic(
+//            (Txn txn) -> usageCount.get()
+//        );
+    }
+
+    public boolean useEnergy(final long units) {
+        return StmUtils.atomic(new TxnBooleanCallable() {
+            @Override
+            public boolean call(Txn txn) throws Exception {
+                long currentLevel = level.get();
+                if (units > 0 && currentLevel >= units) {
+                    level.set(currentLevel - units);
+                    usageCount.increment();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
 //    return  new Atomic<Boolean>() {
 //      public Boolean atomically() {
 //        long currentLevel = level.get();
@@ -60,9 +108,20 @@ public class EnergySource {
 //        }
 //      }
 //    }.execute();
-//  }
-//
-//  private void replenish() {
+    }
+
+    private void replenish() {
+        System.out.println("Running replenish");
+        StmUtils.atomic(() -> {
+            long currentLevel = level.get();
+            System.out.println("currentLevel="+currentLevel);
+            if (currentLevel < MAXLEVEL) {
+                level.increment();
+//                level.set(currentLevel + 1);
+            }
+            System.out.println("Replenish done");
+            return;
+        });
 //    new Atomic() {
 //      public Object atomically() {
 //        long currentLevel = level.get();
@@ -70,5 +129,5 @@ public class EnergySource {
 //        return null;
 //      }
 //    }.execute();
-//  }
+    }
 }
--- using-transactional-memory/src/main/java/com/agiledeveloper/pcj/UseEnergySource.java
+++ using-transactional-memory/src/main/java/com/agiledeveloper/pcj/UseEnergySource.java
@@ -5,44 +5,75 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/vspcon for more book information.
-***/
+ ***/
 package com.agiledeveloper.pcj;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class UseEnergySource {
-//  private static final EnergySource energySource = EnergySource.create();
-//
-//  public static void main(final String[] args)
-//    throws InterruptedException, ExecutionException {
-//    System.out.println("Energy level at start: " +
-//      energySource.getUnitsAvailable());
-//
-//    List<Callable<Object>> tasks = new ArrayList<Callable<Object>>();
-//
-//    for(int i = 0; i < 10; i++) {
-//      tasks.add(new Callable<Object>() {
-//        public Object call() {
-//          for(int j = 0; j < 7; j++) energySource.useEnergy(1);
-//          return null;
-//        }
-//      });
-//    }
-//
-//    final ExecutorService service = Executors.newFixedThreadPool(10);
-//    service.invokeAll(tasks);
-//
-//    System.out.println("Energy level at end: " +
-//      energySource.getUnitsAvailable());
-//    System.out.println("Usage: " + energySource.getUsageCount());
-//
-//    energySource.stopEnergySource();
-//    service.shutdown();
-//  }
+//    private static final EnergySource energySource = EnergySource.create();
+
+    public static void main(final String[] args)
+            throws InterruptedException, ExecutionException {
+
+
+        final EnergySource energySource = EnergySource.create();
+
+        System.out.println("Energy level at start: " +
+                energySource.getUnitsAvailable());
+
+        List<Callable<Object>> tasks = new ArrayList<Callable<Object>>();
+
+        for (int i = 0; i < 10; i++) {
+            tasks.add(new Callable<Object>() {
+                @Override
+                public Object call() {
+                    final Random rnd = new Random();
+                    for (int j = 0; j < 7; j++) {
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(rnd.nextInt(2000));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                        energySource.useEnergy(1);
+                    };
+                    return null;
+                }
+            });
+        }
+
+        final ExecutorService service = Executors.newFixedThreadPool(10);
+        service.invokeAll(tasks);
+
+        System.out.println("Energy level at end: " +
+                energySource.getUnitsAvailable());
+        System.out.println("Usage: " + energySource.getUsageCount());
+
+        energySource.stopEnergySource();
+        service.shutdown();
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!service.awaitTermination(30, TimeUnit.SECONDS)) {
+                System.out.println("Running shutdownNow");
+                service.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!service.awaitTermination(30, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            service.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
+    }
 }
